d818437ebf29d5752f488699544c1c0fd8219782
nayaragracioli <nayaragraciolisa@gmail.com>
2024-01-17 09:53:05 -0300

    feat: Entrega finalizada

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -1,1 +1,31 @@
 SELECT * FROM books;
+
+SELECT * FROM books 
+JOIN books_categories ON books."id" = books_categories."bookId" 
+JOIN categories ON categories."id" =  books_categories."categoryId" WHERE categories."name" = 'Fantasia';
+
+
+SELECT books."id" AS "bookId",
+books."name" AS "bookName",
+books."pages" AS "bookPages",
+books."createdAt" AS "bookCreatedAt",
+books."updateAt" AS "bookUpdateAt",
+categories."id" AS "categoryId",
+categories."name" AS "categoryName",
+categories."createdAt" AS "categoryCreatedAt",
+categories."updateAt" AS "categoryUpdateAt"
+FROM books 
+JOIN books_categories ON books."id" = books_categories."bookId" 
+JOIN categories ON categories."id" =  books_categories."categoryId";
+
+
+SELECT books."id" AS "bookId",
+books."name" AS "bookName",
+books."pages" AS "bookPages",
+books."createdAt" AS "bookCreatedAt",
+books."updateAt" AS "bookUpdateAt",
+authors."id" AS "authorId",
+authors."name" AS "authorName",
+authors."bio" AS "authorBio"
+FROM books 
+JOIN authors ON authors."id" =  books."authorId";
--- sql/updateData.sql
+++ sql/updateData.sql
@@ -1,3 +1,4 @@
 UPDATE books SET "name" = 'Harry Potter e o Prisioneiro de Azkaban' WHERE "id" = 1 RETURNING *;
 
-UPDATE books SET "authorId" = 1 WHERE "id" = 3, "id" = 4 RETURNING *;
+
+UPDATE books SET "authorId" = 1 WHERE "id" IN (3,4) RETURNING *;
